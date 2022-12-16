72de8df77f469c6065c8c94a125d98f9826019da
A-rielox <arielox.ag@gmail.com>
2022-12-16 16:12:33 -0600

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+	private readonly DataContext _context;
+
+	public UsersController(DataContext context)
+	{
+		_context = context;
+	}
+
+	////////////////////////////////////////////////
+	///////////////////////////////////////////////////
+	// GET: api/Users
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+	{
+		var users = await _context.Users.ToListAsync();
+
+		return Ok(users);
+    }
+
+    ////////////////////////////////////////////////
+    ///////////////////////////////////////////////////
+    // GET: api/Users/userId
+    [HttpGet("{id}", Name = "GetUser")]
+	public async Task<ActionResult<AppUser>> GetUser(int id)
+	{
+		var user = await _context.Users.FindAsync(id);
+
+		return Ok(user);
+	}
+
+    ////////////////////////////////////////////////
+    ///////////////////////////////////////////////////
+    // GET: api/Users
+
+
+
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////
+    ///////////////////////////////////////////////////
+    // GET: api/Users
+
+
+
+
+
+}
