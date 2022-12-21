a8ce0340788c767d09542eb7a38219ab0ee35e35
A-rielox <arielox.ag@gmail.com>
2022-12-20 18:08:01 -0600

    lista seccion 9

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -4,15 +4,21 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+
 @NgModule({
    declarations: [],
    imports: [
       CommonModule,
       BsDropdownModule.forRoot(),
+      TabsModule.forRoot(),
       ToastrModule.forRoot({
          positionClass: 'toast-bottom-right',
       }),
+      NgxGalleryModule,
    ],
-   exports: [BsDropdownModule, ToastrModule],
+   exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
 })
 export class SharedModule {}
